db9e3fb56fb477be706a4250a87e2093b16a05c9
Kristen Sundquist <kssundquist@gmail.com>
2018-04-22 15:22:13 -0700

    Add from_array to linked list

--- ruby/linked_list/linked_list.rb
+++ ruby/linked_list/linked_list.rb
@@ -1,15 +1,23 @@
 class LinkedList
-    attr_reader :head
+  attr_reader :head
 
-    def initialize(first_node = nil)
-        @head = first_node
-    end
+  def initialize(first_node = nil)
+    @head = first_node
+  end
 
-    def empty?
-        @head.nil?
+  def self.from_array(array)
+    h = nil
+    array.reverse_each do |value|
+      h = Node.new(value, h)
     end
+    LinkedList.new(h)
+  end
 
-    def nonempty?
-        !empty?
-    end
+  def empty?
+    @head.nil?
+  end
+
+  def nonempty?
+    !empty?
+  end
 end
--- ruby/linked_list/linked_list.spec.rb
+++ ruby/linked_list/linked_list.spec.rb
@@ -12,4 +12,24 @@ class LinkedListSpec < Test::Unit::TestCase
     linked_list = LinkedList.new(node)
     assert(linked_list.nonempty?)
   end
+
+  def test_from_array_empty
+    linked_list = LinkedList.from_array([])
+    assert(linked_list.empty?)
+  end
+
+  def test_from_array
+    array = [1]
+    linked_list = LinkedList.from_array(array)
+    assert_equal(linked_list.head.value, array.first)
+  end
+
+  def test_from_array_keeps_same_order
+    array = [1,2,3]
+    linked_list = LinkedList.from_array(array)
+    assert_equal(linked_list.head.value, array.first)
+    assert_equal(linked_list.head.next_node.value, array[1])
+    assert_equal(linked_list.head.next_node.next_node.value, array[2])
+  end
+
 end
